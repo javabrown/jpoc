fa092cf16cc36787d59a97e983c3ad4d5592a97e
javabrown <getrk@yahoo.com>
2015-02-17 00:24:44 -0500

    Added TEST Hibernate POC

---
+++ hibernate-test/hbm/HibernateUtil.java
@@ -0,0 +1,25 @@
+package hbm;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+  
+public class HibernateUtil {
+  
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+  
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+        	Configuration  configuration = 
+        			new Configuration().configure( "/resources/hibernate.cfg.xml");
+        	return  configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+  
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
---
+++ src/com/jbrown/batch/Test.java
@@ -0,0 +1,9 @@
+package com.jbrown.batch;
+
+import org.springframework.batch.core.repository.support.SimpleJobRepository;
+
+public class Test {
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
